a0f7ad257d3dc0e698f549f50ad9b684883111c1
Mrakdatkom <markryanzipagan3@gmail.com>
2026-09-04 14:18:54 -0700

    variables (let and const)

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 console.log("Hello world.");
 console.log("From script.js file.");
+
+const myName = "Mark";
+let age = 60;
+const number = 9053076455;
+const address = "Purok dos";
+
+console.log(`Name: ${myName}`);
+console.log(`Age: ${age}`);
+console.log(`Number: ${number}`);
+console.log(`Address: ${address}`);
+console.log(67);
